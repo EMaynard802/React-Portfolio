72910d89811b618e6bfc72023b39e2bd520cac55
Eleanor Maynard <el_mynrd@sbcglobal.net>
2020-04-18 21:57:21 -0400

    Add files via upload

---
+++ scripts/seedDB.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Projects collection and inserts the projects below
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolioupdated");
+
+const projectsSeed = [
+
+  // ADD ANOTHER PROJECT
+
+  {
+    title: "Eat Da Burger",
+    linkDeployed: "https://burger-logger-node-app.herokuapp.com/index",
+    linkGithub: "https://github.com/EMaynard802/Burger",
+    imgSrc: "./images/burgerMunch.png",
+    description: "This full stack application allows the user to make a wishlist of burgers to munch, while also storing a list of burgers previously munched."
+  },
+  {
+    title: "Crime-Wave-Data",
+    linkDeployed: "https://shelbypalumbo.github.io/Heat-Wave-Crime-Data/",
+    linkGithub: "https://github.com/shelbypalumbo/Heat-Wave-Crime-Data",
+    imgSrc: "./images/crime.png",
+    description: "For our first group project we were asked to come together as a team to solve a real world problem using data from multiple server-side API requests."
+  },
+  {
+    title: "Employee Profile Generator",
+    linkGithub: "https://github.com/EMaynard802/Template-Engine---Employee-Summary",
+    imgSrc: "./images/profilegen.jpg",
+    description: "Using a NodeJS interface to create a PDF of the information provided from user's information on GitHub."
+  },
+  {
+    title: "Password Generator",
+    linkDeployed: "https://emaynard802.github.io/Password-Generator/.",
+    linkGithub: "https://emaynard802.github.io/Password-Generator/.",
+    imgSrc: "./images/pwGen.png",
+    description: "Password Generator App"
+  },
+  {
+    title: "Weather",
+    linkDeployed: "https://emaynard802.github.io/Weather-Dashboard/",
+    linkGithub: "https://emaynard802.github.io/Weather-Dashboard/",
+    imgSrc: "./images/weather.png",
+    description: "Weather Dashboard App."
+  },
+  {
+    title: "Workout Tracker",
+    linkDeployed: "https://workout--tracker--app.herokuapp.com",
+    linkGithub: "https://github.com/EMaynard802/Workout-Tracker/",
+    imgSrc: "./images/workout.png",
+    description: "Workout Tracker App."
+  }
+];
+
+db.Projects.remove({})
+  .then(() => db.Projects.collection.insertMany(projectsSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
